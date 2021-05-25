653c4884dfdf8acd4da35e78e033e204868addcf
97250 <97250@DESKTOP-H14PI62>
2021-05-25 13:08:41 +0300

    fix task

--- backend/controllers/getUsers.js
+++ backend/controllers/getUsers.js
@@ -1,40 +1,66 @@
+require("dotenv").config();
+
+const axios = require("axios");
+const token = `Bearer ${process.env.BEARER_TOKEN}`;
+const options = {
+  method: "GET",
+  headers: {
+    "Content-type": "application/json",
+    Authorization: token,
+  },
+};
 // Get list of users and request data from twitter api for each user
 // Store the users data in psql database
-const handleGetUsers = async (req, res, client, psqlDB) => {
+const handleGetUsers = async (req, res, psqlDB) => {
   try {
-    console.log(req.body);
     const users = req.body;
-    const usersData = await client.get("users/lookup", users);
 
-    // Can add a function that check if the user alredy exists in the database
-    // If the user exists do the same tasks just instead insert method i can use update method
+    const usersData = await axios.get(
+      `https://api.twitter.com/1.1/users/lookup.json?screen_name=${users.screen_name}`,
+      options
+    );
+
+    // // Can add a function that check if the user alredy exists in the database
+    // // If the user exists do the same tasks just instead insert method i can use update method
 
     const initalDb = await insertTasksToDb(
       psqlDB,
       users.screen_name.split(",")
     );
-
-    const isDataStore = await storeUserData(client, psqlDB, usersData);
-    return res.json(usersData.map((userInfo) => userInfo.screen_name));
+    const isDataStore = await storeUserData(psqlDB, usersData.data);
+    return res.json(usersData.data.map((userInfo) => userInfo.screen_name));
   } catch (error) {
     console.log(error);
     return res.json("Error in server");
   }
 };
 
-// Get users data and tweets and sotre in the database
-// When the task completed for each user it it store in the database that the task is completed
-const storeUserData = async (client, psqlDB, usersData) => {
+// Get whole users data and tweets and sotre in the database
+// When the task completed for each user, when task is completed it stored in the database as completed
+const storeUserData = async (psqlDB, usersData) => {
   const arrayOfPromises = await Promise.all(
     usersData.map(async (userInfo) => {
-      // Get user tweets (trim_user for reducing the object data)
-      const userTweets = await client.get("statuses/user_timeline", {
-        screen_name: userInfo.screen_name,
-        trim_user: 1,
-      });
-
-      const tweetsText = userTweets.map((tweet) => tweet.text) + "";
-      const commonWord = findMostCommonWord(tweetsText);
+      // Sending requests by max_id to get the previus tweets to this id
+      let tweetText = [];
+      let max_id = "";
+      let maxMessages = true;
+      while (maxMessages) {
+        const userTweets = await axios.get(
+          `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${
+            userInfo.screen_name
+          }&count=200&trim_user=1&include_rts=1&&${
+            max_id ? `max_id=${max_id}` : ""
+          }`,
+          options
+        );
+        // If the the length of the tweets data is below 195 stop requesting more tweets
+        if (userTweets.data.length < 195) maxMessages = false;
+        // The max max_id is equal to the last tweet id in the array of tweets
+        max_id = userTweets.data[userTweets.data.length - 1].id;
+        // Can change it to array that start iterating from index 1 instead of 0 for not repating the max_id tweet after the first request
+        tweetText.push(userTweets.data.map((tweet) => tweet.text + ""));
+      }
+      const commonWord = findMostCommonWord(tweetText);
 
       // Using transaction to chain database respones.
       // When user data stored in database then the task is completed and stored in the database(completed=true)
@@ -67,10 +93,10 @@ const storeUserData = async (client, psqlDB, usersData) => {
   return arrayOfPromises;
 };
 // Find most common repeated word and return it
-const findMostCommonWord = (tweetsText) => {
+const findMostCommonWord = (tweetText) => {
   // Can add more filters by words and regex
-  let arrayOfWords = tweetsText.split(" ");
 
+  let arrayOfWords = tweetText.join(" ").split(" ");
   // Algorithm to check maximun repated word
   let wordsMap = new Map();
   let maxNumberOfWords = 1;
@@ -86,7 +112,6 @@ const findMostCommonWord = (tweetsText) => {
       wordsMap.set(word, 1);
     }
   });
-
   return mostCommonWord;
 };
 
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-var Twitter = require("twitter");
 const getUsers = require("./controllers/getUsers");
 const getUserData = require("./controllers/getUserData");
 const knex = require("knex");
@@ -21,17 +20,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-});
-
 app.get("/", (req, res) => res.send("Server is running"));
 // Get users data and store it in database
 app.post("/get-users", (req, res) => {
-  getUsers.handleGetUsers(req, res, client, psqlDB);
+  getUsers.handleGetUsers(req, res, psqlDB);
 });
 // Get specific user data from database
 app.get("/get-specsific-user/:username", (req, res) => {
